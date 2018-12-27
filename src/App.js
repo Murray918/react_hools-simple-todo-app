3aa05189b0ea58728749e4670a1d7cc2107b61f5
Andrew Murray <darbyguitars@gmail.com>
2018-12-26 23:54:13 -0600

    this is the initial commit i have one component so far

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const todos = ([todos, setTodos] = useState([
+    {
+      text: "React Hooks in Depth",
+      isCompleted: false
+    },
+    {
+      text: "Write Articles @ Medium",
+      isCompleted: false
+    },
+    {
+      text: "Share article at Reddit",
+      isCompleted: false
+    }
+  ]));
 }
 
 export default App;
